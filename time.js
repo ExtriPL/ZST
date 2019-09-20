edebb0a77c1a6d79b14768e5db3295db5b265c3d
Mruwawa <mruwa.wa@o2.pl>
2019-09-20 21:19:46 +0200

    Dynamiczna data i godzina

--- time.js
+++ time.js
@@ -1,11 +1,31 @@
 function showTime() {
-var d = new Date();
-document.getElementById('date').innerHTML = data();
-document.getElementById('time').innerHTML = d.getHours() + ":" + d.getMinutes();
+    var d = new Date();
+    document.getElementById('date').innerHTML = data();
+    document.getElementById('time').innerHTML = time();
 }
 
 function data(){
     var d = new Date();
+    var date;
+    var days = ["poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela"];
+    var months = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"];
+
+    date = days[d.getDay() - 1] + ", " + d.getDate() + " " + months[d.getMonth()] + " " + d.getFullYear();
+
+    return date;
 }
 
-window.setInterval('showTime()', 1000);
+function time(){
+    var d = new Date();
+    var time;
+    var minutes;
+
+    minutes = d.getMinutes().toString().length == 2 ? d.getMinutes() : "0" + d.getMinutes();
+
+    time = d.getHours() + ":" + minutes;
+
+    return time;
+}
+showTime();
+
+window.setInterval('showTime()', 10000);
