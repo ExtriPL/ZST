37479b032d621e82bcf87827135880226337bf7f
ExtriPL <extripl@gmail.com>
2019-09-21 21:01:21 +0200

    Zabawy z pogodą

--- weather.js
+++ weather.js
@@ -1,28 +1,36 @@
-let city = 'Radom';
-let weatherRefresInterval = 500;
-let temp, pressure, humidity, temp_max, temp_min, wind_speed, wind_deg;
+let cityId = '7532212';
+let weatherRefreshInterval = 500;
+let temp;//, pressure, humidity, temp_max, temp_min, wind_speed, wind_deg;
 
 function getWeatherData()
 {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=b67b20c37194b3c3764485580c451127', true);
+    let dailyrequest = new XMLHttpRequest();
+    let weatherForecast = new XMLHttpRequest(); //Pogoda na kilka dni
+    dailyrequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&units=metric&appid=b67b20c37194b3c3764485580c451127', true);
+    weatherForecast.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/daily?id=' + cityId + '&cnt=3&appid=7a2866a17c9ee6d742b9e02da5561421', true);
 
-    request.onload = function()
+    dailyrequest.onload = function()
     {
         let data = JSON.parse(this.response);
 
         //Reading data
         temp = data.main.temp; 
-        pressure = data.main.pressure;
+        /*pressure = data.main.pressure;
         humidity = data.main.humidity;
         temp_min = data.main.temp_min;
         temp_max = data.main.temp_max;
         wind_speed = data.wind.speed;
-        wind_deg = data.wind.deg;
+        wind_deg = data.wind.deg;*/
+    }
+    weatherForecast.onload = function()
+    {
+        let data = JSON.parse(this.response);
+        //console.log(data);
     }
 
     //Funkcja wykonywana asynchronicznie
-    request.send();
+    dailyrequest.send();
+    weatherForecast.send();
 }
 
 function reloadWeather()
@@ -34,12 +42,22 @@ function reloadWeather()
 
 function reloadWeatherBar()
 {
-    document.getElementById("temp").innerHTML = Math.round(temp - 273.15) + "°C";
+    document.getElementById("temp").innerHTML = Math.round(temp) + "°C";
 }
 
 function reloadWeatherSlide()
 {
+    let weatherSlide = document.getElementById("weatherSlide");
+    let carousel = document.getElementById("carousel");
 
+    if(weatherSlide != null)
+    {
+        //reload
+    }
+    else
+    {
+        //generate
+    }
 }
 
-setInterval(reloadWeather, weatherRefresInterval);
+setInterval(reloadWeather, weatherRefreshInterval);
