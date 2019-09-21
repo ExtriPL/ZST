d651cebeb906e8708bb12dfaebcba72c2bc978a6
Mruwawa <mruwa.wa@o2.pl>
2019-09-21 20:56:30 +0200

    refresh

---
+++ refresh.js
@@ -0,0 +1,13 @@
+function checkForRefreshFlag() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        if(this.responseText == 'true'){
+          location.reload();
+        }
+      }
+    };
+    xhttp.open("GET", "refresh.php", true);
+    xhttp.send();
+  }
+  setInterval('checkForRefreshFlag()', 5000);
