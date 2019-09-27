150ef8b524821035a3ca5630bc023a1dfc5e6e27
ExtriPL <extripl@gmail.com>
2019-09-27 16:51:39 +0200

    Prace nad slidem z pogodą

--- weather.js
+++ weather.js
@@ -1,36 +1,28 @@
 let cityId = '7532212';
 let weatherRefreshInterval = 500;
-let temp;//, pressure, humidity, temp_max, temp_min, wind_speed, wind_deg;
+let temp, pressure, humidity, temp_max, temp_min, wind_speed, wind_deg;
 
 function getWeatherData()
 {
-    let dailyrequest = new XMLHttpRequest();
-    let weatherForecast = new XMLHttpRequest(); //Pogoda na kilka dni
-    dailyrequest.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&units=metric&appid=b67b20c37194b3c3764485580c451127', true);
-    weatherForecast.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/daily?id=' + cityId + '&cnt=3&appid=7a2866a17c9ee6d742b9e02da5561421', true);
+    let request = new XMLHttpRequest();
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=' + cityId + '&units=metric&appid=b67b20c37194b3c3764485580c451127', true);
 
-    dailyrequest.onload = function()
+    request.onload = function()
     {
         let data = JSON.parse(this.response);
 
         //Reading data
         temp = data.main.temp; 
-        /*pressure = data.main.pressure;
+        pressure = data.main.pressure;
         humidity = data.main.humidity;
         temp_min = data.main.temp_min;
         temp_max = data.main.temp_max;
         wind_speed = data.wind.speed;
-        wind_deg = data.wind.deg;*/
-    }
-    weatherForecast.onload = function()
-    {
-        let data = JSON.parse(this.response);
-        //console.log(data);
+        wind_deg = data.wind.deg;
     }
 
     //Funkcja wykonywana asynchronicznie
-    dailyrequest.send();
-    weatherForecast.send();
+    request.send();
 }
 
 function reloadWeather()
@@ -56,7 +48,7 @@ function reloadWeatherSlide()
     }
     else
     {
-        //generate
+        //carousel.innerHTML += '<div id="weatherSlide" class="carousel-item active h-100"><div class="row h-100"><div class="col-sm h-100" style="background-color:rgb(84, 103, 128)"><div id="weatherImage"></div><div id="weatherTemp"></div><div id="weatherTempMin"></div><div id="weatherTempMax"></div><div id="weatherHumidity"></div><div id="weatherPressure"></div><div id="weatherWindSpeed"></div><div id="weatherWindDeg"></div></div></div></div>';
     }
 }
 
