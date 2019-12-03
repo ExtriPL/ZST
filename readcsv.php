dd4d0e685dd4dd223645169153783f2b42ece5d3
ExtriPL <extripl@gmail.com>
2019-12-03 12:05:02 +0100

    Małe poprawki

--- readcsv.php
+++ readcsv.php
@@ -1,8 +1,10 @@
 <?php
-$newTable = [];
-$allTables = [];
-$scoreTable = [];
-$filesCount = count(glob("csv/"."*"));
+$newTable = []; //Tabela przechowująca najnowsze wyniki
+$allTables = []; //Wszystkie tabele
+$scoreTable = []; //Tabela przechowująca najlepsze wyniki
+$path = "competitions"; //Nazwa folderu przechowującego pliki konkursowe
+$fileName = "konkurs"; //Nazwa pliku, po której mają być rozpoznawane pliki .csv
+$filesCount = count(glob($path."/"."*")); //Liczba plików
 
 //Funkcja sortujące
 function sorting($a, $b)
@@ -10,6 +12,7 @@ function sorting($a, $b)
   return $a[count($a) - 1] < $b[count($b) - 1];
 }
 
+//Sprawdza, który index w tabeli $array odpowiada podanym danym w tabeli $reg, muszą się zgadzać tylko te dane, które są podane w $reg. Kolejność danych nie jest istotna
 function getRegistryIndex($reg, $array)
 {
   $result = -1;
@@ -40,7 +43,7 @@ function getRegistryIndex($reg, $array)
 //Wczytywanie plików
 for($i = 1; $i <= $filesCount; $i++)
 {
-  if (($h = fopen("csv/konkurs".$i.".csv", "r")) !== FALSE) 
+  if (($h = fopen($path."/".$fileName.$i.".csv", "r")) !== FALSE) 
   {
     $temp = [];
     while (($data = fgetcsv($h, 1000, ";")) !== FALSE) 
@@ -53,6 +56,7 @@ for($i = 1; $i <= $filesCount; $i++)
     fclose($h);
   }
 }
+//Uzupełnianie tabeli $newTable najnowszymi danymi
 foreach($allTables[$filesCount - 1] as $value)
 {
   $newTable[] = $value;
@@ -63,22 +67,22 @@ foreach($allTables as $table)
 {
   for($i = 0; $i < count($table); $i++)
   {
+    //Zabezpieczenie przed sortowaniem tytułowego wiersza
     if(strtolower($table[$i][count($table[$i]) - 1]) != "punkty")
     {
-      $reg = [];
+      $reg = []; //Rejestr wpisów, po których ma zostać odnaleziony wpis w tabeli $scoreTable(imie, nazwisko, e-mail itp. ale nie po punktach)
+      //Wypełnianie rejestru odpowiednimi wpisami, ostatnia kolumna jest pomijana(są to punkty)
       for($j = 0; $j < count($table[$i]) - 1; $j++)
       {
         $reg[] = $table[$i][$j];
       }
-      $points = $table[$i][count($table[$i]) - 1];
-      $registryIndex = getRegistryIndex($reg, $scoreTable);
-      if($registryIndex != -1)
+      $points = $table[$i][count($table[$i]) - 1]; //zczytywanie punktów, które następnie przypiszemy, lub dodamy, do odpowiedniego wpisu w tabeli $scoreTable
+      $registryIndex = getRegistryIndex($reg, $scoreTable); //Odnaleziony index wpisu w tabeli $scoreTable
+      if($registryIndex != -1) //Jeżeli index został odnaleziony, dodajemy zdobytą liczbę punktów, do tych, które już znajdują się w tabeli
       {
-        
-        $points2 = $scoreTable[$registryIndex][2];
         $scoreTable[$registryIndex][count($scoreTable[$registryIndex]) - 1] += $points;
       }
-      else
+      else //Jeżeli index nie został odnaleziony, tworzymy nowy wpis
       {
         $reg[] = $points;
         $scoreTable[] = $reg;
@@ -87,8 +91,7 @@ foreach($allTables as $table)
   }
 }
 
-//Sortowanie tabel po liczbie punktów
-usort($newTable, "sorting");
-usort($scoreTable, "sorting");
-array_unshift($scoreTable, $newTable[0]);
+usort($newTable, "sorting"); //Sortowanie tabeli po liczbie punktów
+usort($scoreTable, "sorting"); //Sortowanie tabeli po liczbie punktów
+array_unshift($scoreTable, $newTable[0]); //Dodawanie wiersza z nagłówkami
 ?>
