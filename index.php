31a93f70b33ab3527f3b0a85d1e5ae5c627fbff2
Mruwawa <mruwa.wa@o2.pl>
2020-09-29 21:23:40 +0200

    graed

--- index.php
+++ index.php
@@ -11,13 +11,13 @@
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-        <script src="scripts\galeria.js"></script>
+        <!-- <script src="scripts\galeria.js"></script>  -->
 
         <title>Strona dla ZST</title>
     </head>
     <body>
             
-            
+        <!-- NAVBAR START-->
         <nav id="navbar" class="navbar navbar-expand bg-dark navbar-dark" style="padding-top:0px; padding-bottom:0px;">
             <a class="navbar-brand" href="#">            
                 <h1 id="date"></h1>
@@ -40,24 +40,44 @@
             </ul>
         </nav>
 
-        <div id="demo" class="carousel slide" data-interval="500000000" data-ride="carousel">
-            
-        <!-- style="background-image:url('https://wallpapertag.com/wallpaper/full/7/d/8/593845-snow-background-pictures-1920x1080-hd-1080p.jpg')" -->
+        <!-- NAVBAR END-->
 
-            <div id="carousel" class="carousel-inner h-100">
-            <div class="carousel-item active bck" >
-            <div class="row">
-                <div class="col-md-6"><h1 class="konkurs display-5 text-center">Konkurs 1</h1></div>
-                <div class="col-md-6"><h1 class="konkurs display-5 text-center">Ranking globalny</h1></div>
-            </div>
-            <div class="row" style="height:100%">
-                <div class="col-md-6" style="padding:0px; padding-left:2px; padding-right:2px;"><iframe style="padding-left:15px; width:100%; height:100%; scrolling:no; border:0" src="konkurs1.php"></iframe></div>
-                <div class="col-md-6" style="padding:0px; padding-left:2px; padding-right:18px;"><iframe style="width:100%; height:100%; scrolling:no; border:0" src="konkurs2.php"></iframe></div>
-            </div>
-            </div>    
-            <!-- <div style="clear: both;"></div> -->
-            </div>
-        </div>
+        
+        <div id="demo" class="carousel slide" data-interval="500000000" data-ride="carousel"> 
+            
+            <!-- style="background-image:url('https://wallpapertag.com/wallpaper/full/7/d/8/593845-snow-background-pictures-1920x1080-hd-1080p.jpg')" -->
+
+            <div id="carousel" class="carousel-inner h-100"> <!-- CAROUSEL START-->
+                
+                <div class="carousel-item  slide1 "> <!-- SLIDE 1 START -->
+                    <div class="row">
+                        <div class="col-md-6"><h1 class="konkurs display-5 text-center">Konkurs 1</h1></div>
+                        <div class="col-md-6"><h1 class="konkurs display-5 text-center">Ranking globalny</h1></div>
+                    </div>
+                    <div class="row" style="height:100%">
+                        <div class="col-md-6" style="padding:0px; padding-left:2px; padding-right:2px;"><iframe style="padding-left:15px; width:100%; height:100%; scrolling:no; border:0" src="konkurs1.php"></iframe></div>
+                        <div class="col-md-6" style="padding:0px; padding-left:2px; padding-right:18px;"><iframe style="width:100%; height:100%; scrolling:no; border:0" src="konkurs2.php"></iframe></div>
+                    </div>
+                </div>    <!-- SLIDE 1 END -->
+                
+                <div class="carousel-item slide2  active">
+                    <!-- <div class="row">
+                        <div class="col-md-12">       
+                            <h1 class="display-1 text-white text-center">SPANKO</h1>
+                        </div>
+                    </div>
+                    <div class="row"> -->
+                        <div class="carousel-caption text-white">
+                            <h1>Chce mi sie spac pomocy</h1>
+                        </div>
+                    <!-- </div> -->
+                </div>
+
+
+
+
+            </div> <!-- CAROUSEL END-->
+        </div> 
         <?php
             $fileDir = "images";
             $result = scandir($fileDir);
